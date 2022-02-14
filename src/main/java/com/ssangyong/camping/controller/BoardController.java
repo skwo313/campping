0aaa011156db201c710ad226518e6a86ace1aae7
WonE37 <WonE37@DESKTOP-41GRKB5>
2022-02-14 17:11:56 +0900

    37

--- src/main/java/com/ssangyong/camping/controller/BoardController.java
+++ src/main/java/com/ssangyong/camping/controller/BoardController.java
@@ -19,59 +19,59 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import kr.co.service.BoardService;
-import kr.co.service.ReplyService;
-import kr.co.vo.BoardVO;
-import kr.co.vo.Criteria;
-import kr.co.vo.PageMaker;
-import kr.co.vo.ReplyVO;
-import kr.co.vo.SearchCriteria;
+import com.ssangyong.camping.service.BoardService;
+import com.ssangyong.camping.service.ReplyService;
+import com.ssangyong.camping.vo.BoardVO;
+import com.ssangyong.camping.vo.Criteria;
+import com.ssangyong.camping.vo.PageMaker;
+import com.ssangyong.camping.vo.ReplyVO;
+import com.ssangyong.camping.vo.SearchCriteria;
 
 @Controller
 @RequestMapping("/board/*")
 public class BoardController {
 
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
-	
+
 	@Inject
 	BoardService service;
-	
+
 	@Inject
 	ReplyService replyService;
-	
+
 	// 게시판 글 작성 화면
 	@RequestMapping(value = "/board/writeView", method = RequestMethod.GET)
-	public void writeView() throws Exception{
+	public void writeView() throws Exception {
 		logger.info("writeView");
-		
+
 	}
-	
+
 	// 게시판 글 작성
 	@RequestMapping(value = "/write", method = RequestMethod.POST)
-	public String write(BoardVO boardVO, MultipartHttpServletRequest mpRequest) throws Exception{
+	public String write(BoardVO boardVO, MultipartHttpServletRequest mpRequest) throws Exception {
 		logger.info("write");
 		service.write(boardVO, mpRequest);
-		
+
 		return "redirect:/board/list";
 	}
-	
+
 	// 게시판 목록 조회
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public String list(Model model, @ModelAttribute("scri") SearchCriteria scri) throws Exception{
+	public String list(Model model, @ModelAttribute("scri") SearchCriteria scri) throws Exception {
 		logger.info("list");
-		
+
 		model.addAttribute("list", service.list(scri));
-		
+
 		PageMaker pageMaker = new PageMaker();
 		pageMaker.setCri(scri);
 		pageMaker.setTotalCount(service.listCount(scri));
-		
+
 		model.addAttribute("pageMaker", pageMaker);
-		
+
 		return "board/list";
-		
+
 	}
-	
+
 	// 게시판 조회
 	@RequestMapping(value = "/readView", method = RequestMethod.GET)
 	public String read(BoardVO boardVO, @ModelAttribute("scri") SearchCriteria scri, Model model) throws Exception {
@@ -87,7 +87,7 @@ public class BoardController {
 		model.addAttribute("file", fileList);
 		return "board/readView";
 	}
-	
+
 	// 게시판 수정뷰
 	@RequestMapping(value = "/updateView", method = RequestMethod.GET)
 	public String updateView(BoardVO boardVO, @ModelAttribute("scri") SearchCriteria scri, Model model)
@@ -104,12 +104,10 @@ public class BoardController {
 
 	// 게시판 수정
 	@RequestMapping(value = "/update", method = RequestMethod.POST)
-	public String update(BoardVO boardVO, 
-						 @ModelAttribute("scri") SearchCriteria scri, 
-						 RedirectAttributes rttr,
-						 @RequestParam(value="fileNoDel[]") String[] files,
-						 @RequestParam(value="fileNameDel[]") String[] fileNames,
-						 MultipartHttpServletRequest mpRequest) throws Exception {
+	public String update(BoardVO boardVO, @ModelAttribute("scri") SearchCriteria scri, RedirectAttributes rttr,
+			@RequestParam(value = "fileNoDel[]") String[] files,
+			@RequestParam(value = "fileNameDel[]") String[] fileNames, MultipartHttpServletRequest mpRequest)
+			throws Exception {
 		logger.info("update");
 		service.update(boardVO, files, fileNames, mpRequest);
 
@@ -121,110 +119,111 @@ public class BoardController {
 		return "redirect:/board/list";
 	}
 
-		// 게시판 삭제
-		@RequestMapping(value = "/delete", method = RequestMethod.POST)
-		public String delete(BoardVO boardVO, @ModelAttribute("scri") SearchCriteria scri, RedirectAttributes rttr) throws Exception{
-			logger.info("delete");
-			
-			service.delete(boardVO.getBno());
-			
-			rttr.addAttribute("page", scri.getPage());
-			rttr.addAttribute("perPageNum", scri.getPerPageNum());
-			rttr.addAttribute("searchType", scri.getSearchType());
-			rttr.addAttribute("keyword", scri.getKeyword());
-			
-			return "redirect:/board/list";
-		}
-		
-		//댓글 작성
-		@RequestMapping(value="/replyWrite", method = RequestMethod.POST)
-		public String replyWrite(ReplyVO vo, SearchCriteria scri, RedirectAttributes rttr) throws Exception {
-			logger.info("reply Write");
-			
-			replyService.writedReply(vo);
-			
-			rttr.addAttribute("bno", vo.getBno());
-			rttr.addAttribute("page", scri.getPage());
-			rttr.addAttribute("perPageNum", scri.getPerPageNum());
-			rttr.addAttribute("searchType", scri.getSearchType());
-			rttr.addAttribute("keyword", scri.getKeyword());
-			
-			return "redirect:/board/readView";
-		}
-		
-		//댓글 수정 GET
-		@RequestMapping(value="/replyUpdateView", method = RequestMethod.GET)
-		public String replyUpdateView(ReplyVO vo, SearchCriteria scri, Model model) throws Exception {
-			logger.info("reply Write");
-			
-			model.addAttribute("replyUpdate", replyService.selectReply(vo.getRno()));
-			model.addAttribute("scri", scri);
-			
-			return "board/replyUpdateView";
-		}
-		
-		//댓글 수정 POST
-		@RequestMapping(value="/replyUpdate", method = RequestMethod.POST)
-		public String replyUpdate(ReplyVO vo, SearchCriteria scri, RedirectAttributes rttr) throws Exception {
-			logger.info("reply Write");
-			
-			replyService.updateReply(vo);
-			
-			rttr.addAttribute("bno", vo.getBno());
-			rttr.addAttribute("page", scri.getPage());
-			rttr.addAttribute("perPageNum", scri.getPerPageNum());
-			rttr.addAttribute("searchType", scri.getSearchType());
-			rttr.addAttribute("keyword", scri.getKeyword());
-			
-			return "redirect:/board/readView";
-		}
-		
-		//댓글 삭제 GET
-		@RequestMapping(value="/replyDeleteView", method = RequestMethod.GET)
-		public String replyDeleteView(ReplyVO vo, SearchCriteria scri, Model model) throws Exception {
-			logger.info("reply Write");
-			
-			model.addAttribute("replyDelete", replyService.selectReply(vo.getRno()));
-			model.addAttribute("scri", scri);
-			
+	// 게시판 삭제
+	@RequestMapping(value = "/delete", method = RequestMethod.POST)
+	public String delete(BoardVO boardVO, @ModelAttribute("scri") SearchCriteria scri, RedirectAttributes rttr)
+			throws Exception {
+		logger.info("delete");
 
-			return "board/replyDeleteView";
-		}
-		
-		//댓글 삭제
-		@RequestMapping(value="/replyDelete", method = RequestMethod.POST)
-		public String replyDelete(ReplyVO vo, SearchCriteria scri, RedirectAttributes rttr) throws Exception {
-			logger.info("reply Write");
-			
-			replyService.deleteReply(vo);
-			
-			rttr.addAttribute("bno", vo.getBno());
-			rttr.addAttribute("page", scri.getPage());
-			rttr.addAttribute("perPageNum", scri.getPerPageNum());
-			rttr.addAttribute("searchType", scri.getSearchType());
-			rttr.addAttribute("keyword", scri.getKeyword());
-			
-			return "redirect:/board/readView";
-		}
-		
-		// 첨부파일 다운로드
-		@RequestMapping(value="/fileDown")
-		public void fileDown(@RequestParam Map<String, Object> map, HttpServletResponse response) throws Exception{
-			Map<String, Object> resultMap = service.selectFileInfo(map);
-			String storedFileName = (String) resultMap.get("STORED_FILE_NAME");
-			String originalFileName = (String) resultMap.get("ORG_FILE_NAME");
-			
-			// 파일을 저장했던 위치에서 첨부파일을 읽어 byte[]형식으로 변환한다.
-			byte fileByte[] = org.apache.commons.io.FileUtils.readFileToByteArray(new File("C:\\camping\\file\\"+storedFileName));
-			
-			response.setContentType("application/octet-stream");
-			response.setContentLength(fileByte.length);
-			response.setHeader("Content-Disposition",  "attachment; fileName=\""+URLEncoder.encode(originalFileName, "UTF-8")+"\";");
-			response.getOutputStream().write(fileByte);
-			response.getOutputStream().flush();
-			response.getOutputStream().close();
-			
-		}
-		
-		
+		service.delete(boardVO.getBno());
+
+		rttr.addAttribute("page", scri.getPage());
+		rttr.addAttribute("perPageNum", scri.getPerPageNum());
+		rttr.addAttribute("searchType", scri.getSearchType());
+		rttr.addAttribute("keyword", scri.getKeyword());
+
+		return "redirect:/board/list";
+	}
+
+	// 댓글 작성
+	@RequestMapping(value = "/replyWrite", method = RequestMethod.POST)
+	public String replyWrite(ReplyVO vo, SearchCriteria scri, RedirectAttributes rttr) throws Exception {
+		logger.info("reply Write");
+
+		replyService.writedReply(vo);
+
+		rttr.addAttribute("bno", vo.getBno());
+		rttr.addAttribute("page", scri.getPage());
+		rttr.addAttribute("perPageNum", scri.getPerPageNum());
+		rttr.addAttribute("searchType", scri.getSearchType());
+		rttr.addAttribute("keyword", scri.getKeyword());
+
+		return "redirect:/board/readView";
+	}
+
+	// 댓글 수정 GET
+	@RequestMapping(value = "/replyUpdateView", method = RequestMethod.GET)
+	public String replyUpdateView(ReplyVO vo, SearchCriteria scri, Model model) throws Exception {
+		logger.info("reply Write");
+
+		model.addAttribute("replyUpdate", replyService.selectReply(vo.getRno()));
+		model.addAttribute("scri", scri);
+
+		return "board/replyUpdateView";
+	}
+
+	// 댓글 수정 POST
+	@RequestMapping(value = "/replyUpdate", method = RequestMethod.POST)
+	public String replyUpdate(ReplyVO vo, SearchCriteria scri, RedirectAttributes rttr) throws Exception {
+		logger.info("reply Write");
+
+		replyService.updateReply(vo);
+
+		rttr.addAttribute("bno", vo.getBno());
+		rttr.addAttribute("page", scri.getPage());
+		rttr.addAttribute("perPageNum", scri.getPerPageNum());
+		rttr.addAttribute("searchType", scri.getSearchType());
+		rttr.addAttribute("keyword", scri.getKeyword());
+
+		return "redirect:/board/readView";
+	}
+
+	// 댓글 삭제 GET
+	@RequestMapping(value = "/replyDeleteView", method = RequestMethod.GET)
+	public String replyDeleteView(ReplyVO vo, SearchCriteria scri, Model model) throws Exception {
+		logger.info("reply Write");
+
+		model.addAttribute("replyDelete", replyService.selectReply(vo.getRno()));
+		model.addAttribute("scri", scri);
+
+		return "board/replyDeleteView";
+	}
+
+	// 댓글 삭제
+	@RequestMapping(value = "/replyDelete", method = RequestMethod.POST)
+	public String replyDelete(ReplyVO vo, SearchCriteria scri, RedirectAttributes rttr) throws Exception {
+		logger.info("reply Write");
+
+		replyService.deleteReply(vo);
+
+		rttr.addAttribute("bno", vo.getBno());
+		rttr.addAttribute("page", scri.getPage());
+		rttr.addAttribute("perPageNum", scri.getPerPageNum());
+		rttr.addAttribute("searchType", scri.getSearchType());
+		rttr.addAttribute("keyword", scri.getKeyword());
+
+		return "redirect:/board/readView";
+	}
+
+	// 첨부파일 다운로드
+	@RequestMapping(value = "/fileDown")
+	public void fileDown(@RequestParam Map<String, Object> map, HttpServletResponse response) throws Exception {
+		Map<String, Object> resultMap = service.selectFileInfo(map);
+		String storedFileName = (String) resultMap.get("STORED_FILE_NAME");
+		String originalFileName = (String) resultMap.get("ORG_FILE_NAME");
+
+		// 파일을 저장했던 위치에서 첨부파일을 읽어 byte[]형식으로 변환한다.
+		byte fileByte[] = org.apache.commons.io.FileUtils
+				.readFileToByteArray(new File("C:\\camping\\file\\" + storedFileName));
+
+		response.setContentType("application/octet-stream");
+		response.setContentLength(fileByte.length);
+		response.setHeader("Content-Disposition",
+				"attachment; fileName=\"" + URLEncoder.encode(originalFileName, "UTF-8") + "\";");
+		response.getOutputStream().write(fileByte);
+		response.getOutputStream().flush();
+		response.getOutputStream().close();
+
+	}
+
 }
