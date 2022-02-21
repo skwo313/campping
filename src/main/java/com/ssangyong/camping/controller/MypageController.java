c110dc79605bc9e4f1915bc9911036f6eaf980cd
Gwangwoo Seok <skwo319@naver.com>
2022-02-21 11:14:00 +0900

    1-1

--- src/main/java/com/ssangyong/camping/controller/MypageController.java
+++ src/main/java/com/ssangyong/camping/controller/MypageController.java
@@ -4,18 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/mypage")
 public class MypageController {
 	
-	// http://localhost:7080/camping/userModify.do
-	@RequestMapping("/userModify.do")
+	// http://localhost:7080/camping/mypage?method=userModify
+	@RequestMapping(params="method=userModify")
 	public String mypage() {
 		return "mypage/userModify.page";
 	}
-	@RequestMapping("/userck.do")
+	// http://localhost:7080/camping/mypage?method=userck
+	@RequestMapping(params="method=userck")
 	public String userck() {
 		return "mypage/usercheck.page";
 	}
-	@RequestMapping("/reservations")
+	// http://localhost:7080/camping/mypage?method=reservations
+	@RequestMapping(params="method=reservations")
 	public String reservation() {
 		return "mypage/campsiteList.page";
 	}
