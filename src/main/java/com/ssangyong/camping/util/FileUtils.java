533d28517ae92fc6ae46b3e840b710d7693483bf
WonE37 <WonE37@DESKTOP-41GRKB5>
2022-02-23 16:54:39 +0900

    37

--- src/main/java/com/ssangyong/camping/util/FileUtils.java
+++ src/main/java/com/ssangyong/camping/util/FileUtils.java
@@ -14,7 +14,7 @@ import com.ssangyong.camping.vo.BoardVO;
 
 @Component("fileUtils")
 public class FileUtils {
-	private static final String filePath = "C:\\mp\\file\\"; // 파일이 저장될 위치
+	private static final String filePath = "C:\\camping\\file\\"; // 파일이 저장될 위치
 	
 	public List<Map<String, Object>> parseInsertFileInfo(BoardVO boardVO, 
 			MultipartHttpServletRequest mpRequest) throws Exception{
